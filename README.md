b201afea3fb0d3049c0f290d2705ebeb39ad4d9f
Turyahabwa Mark <profturyahabwa@gmail.com>
2022-11-04 13:04:29 +0300

    init: setup project
    add db interface class
    add config
    add flask with factory pattern

---
+++ README.md
@@ -0,0 +1,4 @@
+# SportsBetx (Microservices)
+Used the microservices architecture pattern. Contains two services:
+A gateway that handles HTTP requests and the application logic that does the odds CRUD.
+
---
+++ sportsbetx/__init__.py
@@ -0,0 +1,27 @@
+"""The application factory"""
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        DATABASE= os.path.join(app.instance_path, 'sportsbetx.sqlite')  # for quick setup will adjust to postgres later
+    )
+
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        env_var = os.getenv('DEPLOY_SETTINGS', 'config.DevelopmentConfig')
+        app.config.from_object(env_var)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except(OSError):
+        pass
+
+    @app.route('/setup') # for quick setup. will be removed
+    def setup():
+        return "Setup route"
+
+    return app
